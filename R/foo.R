15b44f1ed529da606789119cacc35039de4b9b8a
peter shaffery <petersh@ffery.org>
2021-09-10 17:14:31 -0600

    creating utils and placeholder funcs

---
+++ R/foo.R
@@ -0,0 +1,15 @@
+#' Foo
+#' Does nothing! Just a placeholder
+#'
+#'
+#' @param x vector
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' > foo()
+#' NULL
+foo = function(x){
+}
---
+++ R/utils.R
@@ -0,0 +1,21 @@
+# SCALAR FUNCS FOR INTERNAL USE
+
+#' Evaluate the inverse logit function, ie. the logitistic function
+#'
+#' @param z numeric
+#'
+#' @return numeric
+#'
+inv.logit = function(z) {
+  return(1/(1+exp(-z)))
+}
+
+#' Evaluate the 1-alpha/2 quantile of a std normal
+#'
+#' @param alpha numeric
+#'
+#' @return numeric
+#'
+z.alpha = function(alpha){
+  qnorm(1-(alpha/2))
+}
